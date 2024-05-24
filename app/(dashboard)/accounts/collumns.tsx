8f835ea39985ffdbe4011dbc5a3e7fb684563c5d
Diky Arif Febriyanto <dikyariff97@gmail.com>
2024-05-24 19:43:18 +0700

    chore: - add api get individual account data (get) - add api delete individual account data (delete) - add api update individual account data  (patch)

--- app/(dashboard)/accounts/collumns.tsx
+++ app/(dashboard)/accounts/collumns.tsx
@@ -5,8 +5,11 @@ import { ColumnDef } from "@tanstack/react-table";
 import { ArrowUpDown, MoreHorizontal } from "lucide-react";
 
 import { client } from "@/lib/hono";
+
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
+
+import { Actions } from "./actions";
 
 export type ResponseType = InferResponseType<typeof client.api.accounts.$get, 200>["data"][0]
 
@@ -47,4 +50,8 @@ export const columns: ColumnDef<ResponseType>[] = [
       );
     },
   },
+  {
+    id: "actions",
+    cell: ({row}) => <Actions id={row.original.id} />
+  }
 ];
---
+++ features/accounts/api/use-delete-account.ts
@@ -0,0 +1,32 @@
+import { toast } from "sonner";
+import { InferRequestType, InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { client } from "@/lib/hono";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.accounts)[":id"]["$delete"]
+>;
+
+export const useDeleteAccount = (id?: string) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error>({
+    mutationFn: async () => {
+      const res = await client.api.accounts[":id"]["$delete"]({
+        param: { id },
+      });
+      return await res.json();
+    },
+    onSuccess: () => {
+      toast.success("Account Deleted!");
+      queryClient.invalidateQueries({ queryKey: ["account", { id }] });
+      queryClient.invalidateQueries({ queryKey: ["accounts"] });
+    },
+    onError: () => {
+      toast.error("Failed to Delete Account!");
+    },
+  });
+
+  return mutation;
+};
